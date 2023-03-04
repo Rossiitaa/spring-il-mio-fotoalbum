2ea32405c80f63a146373b8320c5c3a373cdecac
Rossiitaa <rosario.scuotto@outlook.it>
2023-03-04 13:33:25 +0100

    user and admin authentication added

---
+++ spring-il-mio-fotoalbum/src/main/java/com/corsojava/fotoalbum/security/DatabaseUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.corsojava.fotoalbum.security;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.corsojava.fotoalbum.model.User;
+import com.corsojava.fotoalbum.repository.UserRepository;
+
+public class DatabaseUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Optional<User> user = userRepository.findByUsername(username);
+		if (user.isPresent()) {
+			return new DatabaseUserDetails(user.get());
+		} else
+			throw new UsernameNotFoundException("Username not found");
+	}
+
+}
